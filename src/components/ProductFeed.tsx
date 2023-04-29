8c099fd084a254ebd9592de9deeb5c4a5860ead0
oliveira-86-c1risk <egberto.duarte.14@gmail.com>
2023-04-29 11:13:41 -0300

    products feed compoenent done

--- src/components/ProductFeed.tsx
+++ src/components/ProductFeed.tsx
@@ -2,7 +2,7 @@ import { FC, useState } from 'react'
 import Products from './Products'
 
 interface ProductItemProps {
-  id: string
+  id: number
   title: string
   price: number
   description: string
@@ -16,17 +16,51 @@ interface ProductFeedProps {
 
 const ProductFeed: FC<ProductFeedProps> = ({ products }) => {
   return (
-    <div>
-      {products.map(({ id, title, price, description, category, image }) => (
-        <Products
-          id={id}
-          title={title}
-          price={price}
-          description={description}
-          category={category}
-          image={image}
-        />
-      ))}
+    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
+      {products
+        .slice(0, 4)
+        .map(({ id, title, price, description, category, image }) => (
+          <Products
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
+
+      <div className="md:col-span-full">
+        <img src="https://links.papareact.com/dyz" alt="middle_banner" />
+      </div>
+
+      <div className="md:col-span-full">
+        {products
+          .slice(4, 5)
+          .map(({ id, title, price, description, category, image }) => (
+            <Products
+              id={id}
+              title={title}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
+            />
+          ))}
+      </div>
+      {products
+        .slice(5, products.length)
+        .map(({ id, title, price, description, category, image }) => (
+          <Products
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
     </div>
   )
 }
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -4,7 +4,7 @@ import { StarIcon } from '@heroicons/react/solid'
 import CurrencyFormat from 'react-currency-format'
 
 interface ProductsProps {
-  id: string
+  id: number
   title: string
   price: number
   description: string
@@ -15,27 +15,27 @@ interface ProductsProps {
 const MAX_RATINGS: number = 5
 const MIN_RATINGS: number = 1
 
-const Products: FC<ProductsProps> = ({
-  id,
-  title,
-  price,
-  description,
-  category,
-  image,
-}) => {
+const Products: FC<ProductsProps> = (
+  { id, title, price, description, category, image },
+  key
+) => {
   const [ratings] = useState(
     Math.floor(Math.random() * (MAX_RATINGS - MIN_RATINGS + 1)) + MIN_RATINGS
   )
-  const [hasPrime] = useState(Math.random() < 0.5)
+  const [hasPrime, setHasPrime] = useState(false)
 
   const [hasMounted, setHasMounted] = useState(false) // <-- add this
 
   useEffect(() => {
     setHasMounted(true)
+    setHasPrime(Math.random() < 0.5)
   }, [])
 
   return (
-    <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+    <div
+      key={key}
+      className="relative flex flex-col justify-between m-5 bg-white z-30 p-10"
+    >
       <div className="absolute top-2 right-2 text-xs italic text-gray-400">
         {category}
       </div>
@@ -48,38 +48,43 @@ const Products: FC<ProductsProps> = ({
         className="self-center"
       />
       <div className="my-3">{title}</div>
+      <div className="mt-7">
+        {hasMounted && (
+          <div className="flex">
+            {Array(ratings)
+              .fill(0)
+              .map((_, index) => (
+                <>
+                  <StarIcon className="h-5 text-yellow-500" />
+                </>
+              ))}
+          </div>
+        )}
 
-      {hasMounted && (
-        <div className="flex">
-          {Array(ratings)
-            .fill(0)
-            .map((_, index) => (
-              <>
-                <StarIcon className="h-5 text-yellow-500" />
-              </>
-            ))}
+        <p className="text-sm my-2 line-clamp-2">{description}</p>
+
+        <div className="mb-5">
+          <CurrencyFormat
+            value={price}
+            displayType={'text'}
+            thousandSeparator={true}
+            prefix={'R$'}
+          />
         </div>
-      )}
 
-      <p className="text-sm my-2 line-clamp-2">{description}</p>
-
-      <div className="mb-5">
-        <CurrencyFormat
-          value={price}
-          displayType={'text'}
-          thousandSeparator={true}
-          prefix={'R$'}
-        />
+        {hasPrime && (
+          <div className="flex items-center space-x-2">
+            <img
+              className="w-12"
+              src="https://links.papareact.com/fdw"
+              alt=""
+            />
+            <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
+          </div>
+        )}
       </div>
 
-      {hasPrime && (
-        <div className="flex items-center space-x-2 -mt-5">
-          <img className="w-12" src="https://links.papareact.com/fdw" alt="" />
-          <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
-        </div>
-      )}
-
-      <button className=" button">Add basket</button>
+      <button className="mt-auto button">Add basket</button>
     </div>
   )
 }
